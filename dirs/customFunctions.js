46e44df3de2337b211251693dadd8ea6c485cc93
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-08-28 16:02:36 +0400

    dqw

--- dirs/customFunctions.js
+++ dirs/customFunctions.js
@@ -23,7 +23,7 @@ module.exports = {
             const server = http.createServer(async (req, res) =>{
                 try {
                     if (req.url.indexOf('/api/auth/google/calendars/token') > -1) {
-                        const qs = new url.URL(req.url, 'https://remindbotsteak.herokuapp.com:80/api/auth/google/calendars/token').searchParams;
+                        const qs = new url.URL(req.url, 'https://remindbotsteak.herokuapp.com/api/auth/google/calendars/token').searchParams;
                         const code = qs.get('code');
                         res.end('Authentication successful! You can now close this window.');
                         server.close();
