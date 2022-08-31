0b9ebb5e5eac1375ad9872d8c148d668f18fe48b
SteakFisher <88953813+SteakFisher@users.noreply.github.com>
2022-08-31 19:28:09 +0400

    Update customFunctions.js

--- dirs/customFunctions.js
+++ dirs/customFunctions.js
@@ -37,7 +37,7 @@ module.exports = {
                         response.end('Authentication successful! You can now close this window.');
 
                         const r = await oAuth2Client.getToken(code);
-                        r["state"] = await oAuth2Client.getToken(qs.get('state'));
+                        r["state"] = qs.get('state');
                         console.log(`TOKENS ARE ${r}`);
                         resolve([r])
                     }
